b9947393a57fc05db2e03991881d4e0c56b8415d
John Breaux <JohnBreaux@my.unt.edu>
2021-10-20 02:26:32 -0500

    Working debug menu! No text output yet.

--- godot_ship/script/game/Main.gd
+++ godot_ship/script/game/Main.gd
@@ -11,7 +11,8 @@ var debug_enabled = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect to signals
-	MessageBus.connect("change_scene", self, "_on_change_scene")
+	MessageBus.connect("change_scene", self, "_on_scene_start")
+	MessageBus.connect("kill_scene", self, "_on_scene_kill")
 	MessageBus.connect("quit", self, "_on_quit_request")
 	MessageBus.connect("return_to_title", self, "_on_title_request")
 	# Create the scenes
@@ -21,21 +22,33 @@ func _ready():
 	debug_menu   = preload("res://scenes/Debug Menu.tscn")
 	if debug_enabled:
 		add_child(debug_menu.instance())
-	_on_change_scene("Title")
+	_on_scene_start("Title")
 
-func _on_change_scene(scene):
+# Creates a new instance of each menu scene
+func _on_scene_start(scene):
+	print ("_on_scene_start(",scene,")")
 	match scene:
-		"Singleplayer":
-			add_child(gameplay.instance())
-		"Multiplayer":
-			add_child(gameplay.instance())
-		"Options":
-			add_child(options.instance())
-		"Title":
-			add_child(title_screen.instance())
+		"Singleplayer": add_child (gameplay.instance())
+		"Multiplayer":  
+			add_child (gameplay.instance())
+			# add_child (multiplayercontroller.instance())
+		"Options":      add_child (options.instance())
+		"Title":        add_child (title_screen.instance())
 
+# Kills all child nodes with name matching `scene`
+func _on_scene_kill(scene):
+	print ("_on_scene_kill(",scene,")")
+	var c = get_children()
+	for i in range (c.size()):
+		if c[i].name == scene:
+			c[i].queue_free()
+
+# Quits
 func _on_quit_request():
+	print ("_on_quit_request()")
 	get_tree().quit()
-	
+
+# Kills the current tree and replaces it with a new one
 func _on_title_request():
-	get_tree().change_scene("res://scenes/Main.tscn")
+	print ("_on_title_request()")
+	return get_tree().change_scene("res://scenes/Main.tscn")
--- godot_ship/script/game/Message Bus.gd
+++ godot_ship/script/game/Message Bus.gd
@@ -3,7 +3,11 @@ extends Node
 # Ask for a scene change
 signal change_scene(scene_name)
 # Ask to kill scene
+signal kill_scene(scene_name)
 # Ask to quit the game
 signal quit
 # Ask to return to title screen
 signal return_to_title
+
+# Ask to print a string to debug console
+signal print_console(string)
