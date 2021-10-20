e41231da24c709727193972914609108bd96a074
ValliantStorme <valliantstorme@gmail.com>
2021-10-20 00:40:31 -0500

    Overhaul menu system

---
+++ godot_ship/script/game/Message Bus.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Ask for a scene change
+signal change_scene(scene_name)
+# Ask to quit the game
+signal quit
+# Ask to return to title screen
+signal return_to_title
--- godot_ship/script/title screen/Title Screen.gd
+++ godot_ship/script/title screen/Title Screen.gd
@@ -1,9 +1,7 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here:
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +15,17 @@ func _ready():
 
 
 func _on_Singleplayer_pressed():
-	get_tree().change_scene("res://scenes/Gameplay.tscn")
+	#get_tree().change_scene("res://scenes/Gameplay.tscn")
+	MessageBus.emit_signal("change_scene", "Singleplayer")
 	
 func _on_Multiplayer_pressed():
-	get_tree().change_scene("res://scenes/Gameplay.tscn")
+	#get_tree().change_scene("res://scenes/Gameplay.tscn")
+	MessageBus.emit_signal("change_scene", "Multiplayer")
 
 func _on_Options_pressed():
-	get_tree().change_scene("res://scenes/Options.tscn")
+	#get_tree().change_scene("res://scenes/Options.tscn")
+	MessageBus.emit_signal("change_scene", "Options")
 
 func _on_Quit_pressed():
-	get_tree().quit()
+	#get_tree().quit()
+	MessageBus.emit_signal("quit")
