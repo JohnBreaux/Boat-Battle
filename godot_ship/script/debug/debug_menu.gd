82501d09b1866044e34ca6b68d75ee7b69125e52
ValliantStorme <valliantstorme@gmail.com>
2021-10-20 02:26:32 -0500

    Working debug menu! No text output yet.

--- godot_ship/script/debug/debug_menu.gd
+++ godot_ship/script/debug/debug_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
 # Declare member variables here.
+var debug_output
+var debug_line = 0
+
 var debug_canvas
 var debug_transform
 
@@ -9,13 +12,15 @@ var menu_position = 0.0
 var menu_velocity = 4
 
 # positions when the menu is hidden/active
-var menu_hidden = Transform2D(Vector2(1,0), Vector2(0,1), Vector2(0,-180))
+var menu_hidden = Transform2D(Vector2(1,0), Vector2(0,1), Vector2(0,-170))
 var menu_active = Transform2D(Vector2(1,0), Vector2(0,1), Vector2(0,   0))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	debug_canvas = get_node("debug_canvas")
 	debug_transform = debug_canvas.get_transform()
+	debug_output = get_node("debug_canvas/VBoxContainer/TextEdit")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -33,8 +38,26 @@ func _unhandled_input(event):
 		# open debug menu
 		debug_active = !debug_active;
 
+func _on_LineEdit_text_entered(line):
+	var command = line.split(' ', true, 1)
+	if command.size() > 0:
+		print("match ", command)
+		match command[0]:
+			"open":
+				if command.size() > 1:
+					MessageBus.emit_signal("change_scene", command[1])
+				else:
+					debug_print_line("Usage: open scene")
+			"kill":
+				if command.size() > 1:
+					MessageBus.emit_signal("kill_scene", command[1])
+				else:
+					debug_print_line("Usage: kill scene")
+			"restart":
+				MessageBus.emit_signal("return_to_title")
+	else:
+		pass
 
-
-func _on_LineEdit_text_entered(new_text):
-	
-	pass # Replace with function body.
+func debug_print_line(string):
+	debug_output.set_line(debug_line, string)
+	debug_line += 1
