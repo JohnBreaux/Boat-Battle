f40bf520d4121a25c422d9f16ed6e27fe016cdb9
John Breaux <JohnBreaux@my.unt.edu>
2021-11-12 01:57:15 -0600

    Collision is good

--- godot_ship/script/game/Gameplay.gd
+++ godot_ship/script/game/Gameplay.gd
@@ -1,23 +1,9 @@
 extends Control
 
-signal two_ship_collide
-signal three_shipA_collide
-signal three_shipB_collide
-signal four_ship_collide
-signal five_ship_collide
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if find_next_valid_focus(): find_next_valid_focus().grab_focus()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Forfeit_pressed():
@@ -25,24 +11,3 @@ func _on_Forfeit_pressed():
 	queue_free();
 	MessageBus.emit_signal("change_scene", "Title")
 
-
-func _on_2Ship_body_entered(body):
-	var _errno = emit_signal("two_ship_collide", "2Ship")
-	print("Emitting two_ship_collide")
-	
-func _on_3ShipA_body_entered(body):
-	emit_signal("three_shipA_collide", "3ShipA")
-	print("Emitting three_shipA_collide")
-	
-func _on_3ShipB_body_entered(body):
-	emit_signal("three_shipB_collide", "3ShipA")
-	print("Emitting three_shipB_collide")
-	
-func _on_4Ship_body_entered(body):
-	emit_signal("four_ship_collide", "4Ship")
-	print("Emitting four_ship_collide")
-
-func _on_5Ship_body_entered(body):
-	emit_signal("five_ship_collide", "5Ship")
-	print("Emitting five_ship_collide")
-	
