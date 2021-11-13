2f5a2cd47f3dbea34cfbe2409c631150b25d0e01
John Breaux <JohnBreaux@my.unt.edu>
2021-11-13 05:08:53 -0600

    Comment on which parts of Game are only there for testing purposes.

--- godot_ship/script/game/Gameplay/Game.gd
+++ godot_ship/script/game/Gameplay/Game.gd
@@ -6,7 +6,9 @@ class ShipData:
 	var Orientation: bool #vertical is true, (Trueship = vertical) (Falseship = horizontal)
 
 # Preloaded assets, to be used later
+# TODO: Move Setup into the Player. It's just here, for now, so that it can be tested and the game doesn't appear broken
 onready var Setup = preload("res://scenes/Game/Setup.tscn")
+# TODO: Move Fire into the Player. See above.
 onready var Fire  = preload("res://scenes/Game/Fire.tscn")
 
 # Path to Player class, for instantiating new Players in code
@@ -25,12 +27,15 @@ var is_multiplayer = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# TODO: Move Setup into the Player.
 	var setup = Setup.instance()
 	setup.connect("game_ready", self, "game_setup")
 	add_child(setup)
 
+# TODO: Move Setup into the Player.
 func game_setup(_ships):
 	print_debug("Congrats! Setup complete.")
+	# TODO: Move Fire into the Player.
 	add_child(Fire.instance())
 
 # Member functions:
